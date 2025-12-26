ae64ca22ac16fee463f5dffb7a2078dd2cca4658
merlin yesod <merlin@MacBook-Pro-de-merlin.local>
2025-12-26 02:12:06 +0000

    Retirer 'src' du suivi Git

---
+++ db.js
@@ -0,0 +1,25 @@
+// src/db.js
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// Test de connexion (optionnel)
+async function testConnection() {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    console.log("Database connection successful:", result.rows);
+  } catch (error) {
+    console.error("Error connecting to the database:", error.message);
+  }
+}
+
+testConnection();
+
+export default pool; // <-- export default pour ESModule
