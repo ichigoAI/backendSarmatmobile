9f1c4c0ead0828d2d1d92ca6050d3b8ebde342c0
merlin yesod <merlin@MacBook-Pro-de-merlin.local>
2025-12-27 22:44:06 +0000

    db upgrade

--- db.js
+++ db.js
@@ -1,18 +1,23 @@
 // src/db.js
 import pkg from "pg";
 import dotenv from "dotenv";
+import { parse } from "pg-connection-string";
 
 dotenv.config();
 
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  family: 4, 
-});
+// Parse l'URL pour passer les options individuellement
+const config = parse(process.env.DATABASE_URL);
+config.ssl = { rejectUnauthorized: false };
+config.host = config.host; // hôte Supabase
+config.port = parseInt(config.port);
+config.user = config.user;
+config.password = config.password;
+config.database = config.database;
+config.family = 4; // FORCE IPv4
+
+const pool = new Pool(config);
 
 async function testConnection() {
   try {
