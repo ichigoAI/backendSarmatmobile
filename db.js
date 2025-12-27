58ec62667e9c2aedb24356af3d0d81ef3ef57b1a
merlin yesod <merlin@MacBook-Pro-de-merlin.local>
2025-12-27 22:37:30 +0000

    db upgrade

--- db.js
+++ db.js
@@ -8,18 +8,20 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-// Test de connexion (optionnel)
 async function testConnection() {
   try {
     const result = await pool.query("SELECT NOW()");
     console.log("Database connection successful:", result.rows);
   } catch (error) {
-    console.error("Error connecting to the database:", error.message);
+    console.error("Error connecting to the database:", error);
   }
 }
 
 testConnection();
 
-export default pool; // <-- export default pour ESModule
+export default pool;
